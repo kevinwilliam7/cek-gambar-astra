a1e43d1f4ee41a6b827c606286005e583276a2b9
Kevin William <kevonwojdkzjd@gmail.com>
2025-12-28 20:30:42 +0700

    fix update or create webc

--- app/Imports/WebcImport.php
+++ app/Imports/WebcImport.php
@@ -36,30 +36,35 @@ class WebcImport implements OnEachRow, WithHeadingRow, WithChunkReading
             return;
         }
 
-        AstraWebc::updateOrCreate(
-            [
-                'nama_region' => $data['nama_region'],
-                'nomor_pkb' => $data['pkb_number'],
-                'nama_ahass' => $data['nama_ahass'],
-                'nomor_transaksi' => $data['nomor_transaksi'],
-                'kode_ahass' => $data['kode_ahass'],
-                'nama_customer' => $data['nama_customer'] ?? null,
-                'no_handphone' => $data['no_handphone'] ?? null,
-                "type_motor" => $data['type_motor'] ?? null,
-                "nomor_mesin" => $data['no_mesin'] ?? null,
-                "no_polisi" => $data['no_polisi'] ?? null,
-                "kpb_type" => $data['kpb_type'] ?? null,
-                "km" => $data['km'] ?? null,
-                "tanggal_beli" => $data['tanggal_beli'] ?? null,
-                "tanggal_claim" => $data['tanggal_claim'] ?? null,
-                "validitas" => $data['validitas'],
-                "no_rangka" => $data['no_rangka'] ?? null,
-                "filename" => $data['photo_url'],
-            ],
-            [
-                "current_excel" => strtolower($sheetName) === 'sheet2' ? true : false,
-            ]
-        );
+        if (str_contains($data['photo_url'], 'https') === true) {
+            AstraWebc::updateOrCreate(
+                [
+                    'kode_ahass' => $data['kode_ahass'],
+                    "nomor_mesin" => $data['no_mesin'] ?? null,
+                    "kpb_type" => $data['kpb_type'] ?? null,
+                    "km" => $data['km'] ?? null,
+                    'nomor_pkb' => $data['pkb_number'] ?? null,
+                ],
+                [
+                    'nama_region' => $data['nama_region'],
+                    'nama_ahass' => $data['nama_ahass'],
+                    'nomor_transaksi' => $data['nomor_transaksi'],
+                    'nama_customer' => $data['nama_customer'] ?? null,
+                    'no_handphone' => $data['no_handphone'] ?? null,
+                    "type_motor" => $data['type_motor'] ?? null,
+                    "no_polisi" => $data['no_polisi'] ?? null,
+                    "tanggal_beli" => $data['tanggal_beli'] ?? null,
+                    "tanggal_claim" => $data['tanggal_claim'] ?? null,
+                    "validitas" => $data['validitas'],
+                    "no_rangka" => $data['no_rangka'] ?? null,
+                    "filename" => $data['photo_url'],
+                ],
+            );
+        } else {
+            if ($this->command) {
+                $this->command->warn($sheetName . " " . $this->year . "_" . $this->month . " Skip import webc row: " . ($data['no_mesin'] ?? '') . " karena photo_url tidak valid");
+            }
+        }
 
 
         if ($this->command) {
