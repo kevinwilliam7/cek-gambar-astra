75085569b8a96d41c31930fab50ea5f93fa23380
Kevin William <kevonwojdkzjd@gmail.com>
2026-01-01 17:23:37 +0700

    add css and js datatables

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,5 @@ import $ from 'jquery';
 window.$ = window.jQuery = $;
 
 import 'datatables.net-dt'; // core DataTables JS
+import 'datatables.net-colreorder-dt';   // ColReorder extension
+import 'datatables.net-fixedheader';  // FixedHeader
